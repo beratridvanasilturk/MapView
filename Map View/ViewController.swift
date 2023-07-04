3e218eed87e32d38fff9690e85529c3ee986b27e
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-04 18:08:54 +0300

    Added Annotation (Location Pin) for Map Kit

--- Map View/ViewController.swift
+++ Map View/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     //MARK: Variables
     @IBOutlet var mapView: MKMapView!
     
+    // alanin yaricapi
     let regionRadius = 3000
     let location = CLLocation(latitude: 36.2972, longitude: 30.1460)
     
@@ -20,13 +21,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         centreMap(location: location)
+        
+        
     }
     
     private func centreMap(location: CLLocation) {
-        
+        // Alanlari belirler
         let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: CLLocationDistance(floatLiteral: Double(regionRadius)), longitudinalMeters: CLLocationDistance(floatLiteral: Double(regionRadius)))
         
+        let coorType = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coorType
+        
+        annotation.title = "Best View"
+        annotation.subtitle = "to visit"
+
+        mapView.addAnnotation(annotation)
         mapView.setRegion(region, animated: true)
+        
     }
 }
 
